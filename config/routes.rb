60b5c120c9d45a0673cea786dcc51ce587e648e9
Dan <DanielMooreRealPerson@gmail.com>
2021-04-30 13:55:09 -0400

    updated after a few days, sessions up and running

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+  resources :reviews
+  root 'sessions#home'
+  get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
+  get 'users/:id', to: 'users#show', as: "user"
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+  resources :books
+  resources :users
+  resources :reviews
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
